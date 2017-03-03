4223ebd5772ed8d40acbaa6f186ff4cdae13122c
PacGyver <oliver@familie-warzecha.de>
2017-03-03 19:34:24 +0100

    Add files via upload

--- tv_power.sh
+++ tv_power.sh
@@ -2,10 +2,13 @@
 OUTPUT="/var/state/tvpower"
 SLEEP=5
 DEVICE=0
-(while true ; do sleep $SLEEP ; echo "pow $DEVICE" ; done) |cec-client -d 8 -p 1 -b 5 -t p | grep power  --line-buffered | while read x ; do	
+STATE=""
+
+(while true; do sleep $SLEEP ; echo "pow $DEVICE"; done) | cec-client -d 8 -p 1 -b 5 -t p | grep power  --line-buffered | while read x ; do	
 	STATE=`echo $x | awk '{split($0,a,":");print a[2]}' | sed 's/ //'`
-	if [ $STATE == "on" -o $STATE == "standby" ]
+	if [ $STATE == "on" -o $STATE == "standby" ] && [ ! "$STATE" == "$PREV_STATE" ]
 	then
-		echo $STATE | tee $OUTPUT
+			echo $STATE | tee $OUTPUT
+			PREV_STATE=$STATE
 	fi
 done
